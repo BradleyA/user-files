4e853a49aa9eec6941a7b64185d2b27868f68ee2
Bradley Allen <allen.bradley@ymail.com>
2018-09-12 20:09:37 -0500

    added my perferred terminal window size and layout for screen one

---
+++ bin/term-layout.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# 	term-layout.sh  2.12.48  2018-09-12_20:06:09_CDT  https://github.com/BradleyA/user-work-files.git  uadmin  six-rpi3b.cptx86.com 2.11  
+# 	   added my perferred terminal window size and layout for screen one 
+###
+#	six terminal window 132x43 from lower left to upper right
+###
+gnome-terminal --geometry=132x42+10+490
+gnome-terminal --geometry=132x42+220+400
+gnome-terminal --geometry=132x42+380+310
+gnome-terminal --geometry=132x42+540+220
+gnome-terminal --geometry=132x42+700+130
+gnome-terminal --geometry=132x42+860+40
+###
