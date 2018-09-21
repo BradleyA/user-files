8b7db964e9edc64be825aae98314207fe94f5847
Bradley Allen <allen.bradley@ymail.com>
2018-09-20 22:09:22 -0500

    rename get_date_stamp

--- template/template.py
+++ template/template.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+# 	template.py  3.34.73  2018-09-20_22:09:10_CDT  https://github.com/BradleyA/user-work-files.git  uadmin  six-rpi3b.cptx86.com 3.33  
+# 	   rename get_date_stamp 
 # 	template.py  3.33.72  2018-09-20_21:47:55_CDT  https://github.com/BradleyA/user-work-files.git  uadmin  six-rpi3b.cptx86.com 3.32  
 # 	   update import scrollphat 
 # 	template.py  3.32.71  2018-09-20_21:18:35_CDT  https://github.com/BradleyA/user-work-files.git  uadmin  six-rpi3b.cptx86.com 3.31  
@@ -29,7 +31,7 @@ def display_help() :
    print ("   {} <<code example goes here>>\n".format(__file__))
 #  After displaying help in english check for other languages
    if LANGUAGE != "en_US.UTF-8" :
-      print ("{}{} {} {}[WARNING]{}  {}  Your language, {} is not supported, Would you like to help?".format(color.END, __file__, get_line_no(), color.BOLD, color.END, get_time_stamp(), LANGUAGE))
+      print ("{}{} {} {}[WARNING]{}  {}  Your language, {} is not supported, Would you like to help?".format(color.END, __file__, get_line_no(), color.BOLD, color.END, get_date_stamp(), LANGUAGE))
 #  elif LANGUAGE != "fr_CA.UTF-8" :
 #     print display_help in french
 #  else :
@@ -41,8 +43,8 @@ def get_line_no() :
    cf = currentframe()
    return cf.f_back.f_lineno
 
-#  date and time function
-def get_time_stamp() :
+#  Date and time function
+def get_date_stamp() :
    return time.strftime("%Y-%m-%d-%H-%M-%S-%Z")
 
 #  Default help and version arguments
@@ -63,43 +65,43 @@ if no_arguments == 2 :
 
 #  DEBUG example
 import platform
-if DEBUG == 1 : print ("> {}DEBUG{} {}  {}  Name of command >{}<  Version of python >{}<".format(color.BOLD, color.END, get_line_no(), get_time_stamp(), __file__, platform.python_version()))
+if DEBUG == 1 : print ("> {}DEBUG{} {}  {}  Name of command >{}<  Version of python >{}<".format(color.BOLD, color.END, get_line_no(), get_date_stamp(), __file__, platform.python_version()))
 
 ###	#  Examples -->
 #  if argument; use argument -> do not use default or environment variables for MESSAGE
 if no_arguments == 2 :
 #  Set non-default MESSAGE file including path
    MESSAGE = sys.argv[1]
-   if DEBUG == 1 : print ("> {}DEBUG{} {}  {}  Using MESSAGE file >{}<".format(color.BOLD, color.END, get_line_no(), get_time_stamp(), MESSAGE))
+   if DEBUG == 1 : print ("> {}DEBUG{} {}  {}  Using MESSAGE file >{}<".format(color.BOLD, color.END, get_line_no(), get_date_stamp(), MESSAGE))
 else :
 #  if no argument; -> use default if environment variables not defined
    #  Check DATA_DIR; set using os environment variable
    if "DATA_DIR" in os.environ :
       DATA_DIR = os.getenv("DATA_DIR")
-      if DEBUG == 1 : print ("> {}DEBUG{} {}  {}  Using environment variable DATA_DIR >{}<".format(color.BOLD, color.END, get_line_no(), get_time_stamp(), DATA_DIR))
+      if DEBUG == 1 : print ("> {}DEBUG{} {}  {}  Using environment variable DATA_DIR >{}<".format(color.BOLD, color.END, get_line_no(), get_date_stamp(), DATA_DIR))
    else :
    #  Set DATA_DIR with default
       DATA_DIR = "/usr/local/data/"
-      if DEBUG == 1 : print ("> {}DEBUG{} {}  {}  Environment variable DATA_DIR NOT set, using default >{}<".format(color.BOLD, color.END, get_line_no(), get_time_stamp(), DATA_DIR))
+      if DEBUG == 1 : print ("> {}DEBUG{} {}  {}  Environment variable DATA_DIR NOT set, using default >{}<".format(color.BOLD, color.END, get_line_no(), get_date_stamp(), DATA_DIR))
    if "CLUSTER" in os.environ :
    #  Check CLUSTER; set using os environment variable
       CLUSTER = os.getenv("CLUSTER")
-      if DEBUG == 1 : print ("> {}DEBUG{} {}  {}  Using environment variable CLUSTER >{}<".format(color.BOLD, color.END, get_line_no(), get_time_stamp(), CLUSTER))
+      if DEBUG == 1 : print ("> {}DEBUG{} {}  {}  Using environment variable CLUSTER >{}<".format(color.BOLD, color.END, get_line_no(), get_date_stamp(), CLUSTER))
    else :
    #  Set CLUSTER with default
       CLUSTER = "us-tx-cluster-1/"
-      if DEBUG == 1 : print ("> {}DEBUG{} {}  {}  Environment variable CLUSTER NOT set, using default >{}<".format(color.BOLD, color.END, get_line_no(), get_time_stamp(), CLUSTER))
+      if DEBUG == 1 : print ("> {}DEBUG{} {}  {}  Environment variable CLUSTER NOT set, using default >{}<".format(color.BOLD, color.END, get_line_no(), get_date_stamp(), CLUSTER))
    if "MESSAGE_FILE" in os.environ :
    #  Check MESSAGE_FILE; set using os environment variable
       MESSAGE_FILE = os.getenv("MESSAGE_FILE")
-      if DEBUG == 1 : print ("> {}DEBUG{} {}  {}  Using environment variable MESSAGE_FILE >{}<".format(color.BOLD, color.END, get_line_no(), get_time_stamp(), MESSAGE_FILE))
+      if DEBUG == 1 : print ("> {}DEBUG{} {}  {}  Using environment variable MESSAGE_FILE >{}<".format(color.BOLD, color.END, get_line_no(), get_date_stamp(), MESSAGE_FILE))
    else :
    #  Set MESSAGE_FILE with default
       MESSAGE_FILE = "MESSAGE"
-      if DEBUG == 1 : print ("> {}DEBUG{} {}  {}  Environment variable MESSAGE_FILE NOT set, using default >{}<".format(color.BOLD, color.END, get_line_no(), get_time_stamp(), MESSAGE_FILE))
+      if DEBUG == 1 : print ("> {}DEBUG{} {}  {}  Environment variable MESSAGE_FILE NOT set, using default >{}<".format(color.BOLD, color.END, get_line_no(), get_date_stamp(), MESSAGE_FILE))
    #  Set MESSAGE with absolute path
    MESSAGE = DATA_DIR + CLUSTER + MESSAGE_FILE
-if DEBUG == 1 : print ("> {}DEBUG{} {}  {}  Using MESSAGE file >{}<".format(color.BOLD, color.END, get_line_no(), get_time_stamp(), MESSAGE))
+if DEBUG == 1 : print ("> {}DEBUG{} {}  {}  Using MESSAGE file >{}<".format(color.BOLD, color.END, get_line_no(), get_date_stamp(), MESSAGE))
 ###
 
 #  Check if two arguments after command if TRUE save second argument 
@@ -108,21 +110,21 @@ if no_arguments == 3 :
    if sys.argv[1] = "-f" :
 #     Set FILE_NAME to second argument
       FILE_NAME = sys.argv[2]
-      if DEBUG == 1 : print ("> {}DEBUG{} {}  {}  Found -f FILE_NAME >{}<".format(color.BOLD, color.END, get_line_no(), get_time_stamp(), FILE_NAME))
+      if DEBUG == 1 : print ("> {}DEBUG{} {}  {}  Found -f FILE_NAME >{}<".format(color.BOLD, color.END, get_line_no(), get_date_stamp(), FILE_NAME))
    elif sys.argv[1] = "-h" :
 #     Set USERHOME to second argument
       USERHOME = sys.argv[2]
-      if DEBUG == 1 : print ("> {}DEBUG{} {}  {}  Found -h USERHOME >{}<".format(color.BOLD, color.END, get_line_no(), get_time_stamp(), USERHOME))
+      if DEBUG == 1 : print ("> {}DEBUG{} {}  {}  Found -h USERHOME >{}<".format(color.BOLD, color.END, get_line_no(), get_date_stamp(), USERHOME))
    else :
 #     Unknow option print ERROR
-      print ("\n{}{} {} {}[ERROR]{}  {}  Unsupported option >{}<".format(color.END, __file__, get_line_no(), color.BOLD, color.END, get_time_stamp(), sys.argv[1]))
+      print ("\n{}{} {} {}[ERROR]{}  {}  Unsupported option >{}<".format(color.END, __file__, get_line_no(), color.BOLD, color.END, get_date_stamp(), sys.argv[1]))
       sys.exit()
 
 #  Check for argument 2 and save
 if no_arguments == 3 :
    OPTION1 = sys.argv[1]
    OPTION2 = sys.argv[2]
-   if DEBUG == 1 : print ("> {}DEBUG{} {}  {}  Using OPTION1 >{}<  OPTION2 >{}<".format(color.BOLD, color.END, get_line_no(), get_time_stamp(), OPTION1, OPTION2))
+   if DEBUG == 1 : print ("> {}DEBUG{} {}  {}  Using OPTION1 >{}<  OPTION2 >{}<".format(color.BOLD, color.END, get_line_no(), get_date_stamp(), OPTION1, OPTION2))
 
 #  Examples
 print ("\nNumber of arguments: {} arguments.".format(len(sys.argv)))
@@ -131,7 +133,7 @@ print ("command = {}".format(sys.argv[0]))
 
 #  Read TEMP_FILE contents and return contents
 def get_msg(TEMP_FILE) :
-   if DEBUG == 1 : print ("> {}DEBUG{} {}  {}  Reading MESSAGE file >{}<".format(color.BOLD, color.END, get_line_no(), get_time_stamp(), TEMP_FILE))
+   if DEBUG == 1 : print ("> {}DEBUG{} {}  {}  Reading MESSAGE file >{}<".format(color.BOLD, color.END, get_line_no(), get_date_stamp(), TEMP_FILE))
    file = open(TEMP_FILE,"r")
    CONTENT = file.read()
    file.close()
@@ -142,10 +144,10 @@ lines = get_msg(MESSAGE)
 #
 
 #  ERROR example		
-print ("\n{}{} {} {}[ERROR]{}  {}  USER don't do that!".format(color.END, __file__, get_line_no(), color.BOLD, color.END, get_time_stamp()))
+print ("\n{}{} {} {}[ERROR]{}  {}  USER don't do that!".format(color.END, __file__, get_line_no(), color.BOLD, color.END, get_date_stamp()))
 
 #  INFO example
-print ("\n{}{} {} {}[INFO]{}  {}  Using OPTION >{}<".format(color.END, __file__, get_line_no(), color.BOLD, color.END, get_time_stamp(), OPTION3))
+print ("\n{}{} {} {}[INFO]{}  {}  Using OPTION >{}<".format(color.END, __file__, get_line_no(), color.BOLD, color.END, get_date_stamp(), OPTION3))
 
 #  Examples	
 print ("\ncommand with path = {}".format(__file__))
@@ -157,18 +159,18 @@ from os import getcwd
 print ("pwd = {}".format(getcwd()))
 
 #  Example Done
-print ("\n{}{} {} {}[INFO]{}  {}  Done.\n".format(color.END, __file__, get_line_no(), color.BOLD, color.END, get_time_stamp()))
+print ("\n{}{} {} {}[INFO]{}  {}  Done.\n".format(color.END, __file__, get_line_no(), color.BOLD, color.END, get_date_stamp()))
 
 #  import scrollphat and check if scrollphat installed 
 try :
    import scrollphat
 except ImportError :
    if sys.version_info[0] < 3 :
-      sys.exit("\n{}{} {} {}[ERROR]{}  {}  Library scrollphat required. To install:\n\tsudo apt-get install python-scrollphat".format(color.END, __file__, get_line_no(), color.BOLD, color.END, get_time_stamp()))
+      sys.exit("\n{}{} {} {}[ERROR]{}  {}  Library scrollphat required. To install:\n\tsudo apt-get install python-scrollphat".format(color.END, __file__, get_line_no(), color.BOLD, color.END, get_date_stamp()))
    elif sys.version_info[0] == 3 :
-      sys.exit("\n{}{} {} {}[ERROR]{}  {}  Library scrollphat required. To install:\n\tsudo apt-get install python3-scrollphat".format(color.END, __file__, get_line_no(), color.BOLD, color.END, get_time_stamp()))
+      sys.exit("\n{}{} {} {}[ERROR]{}  {}  Library scrollphat required. To install:\n\tsudo apt-get install python3-scrollphat".format(color.END, __file__, get_line_no(), color.BOLD, color.END, get_date_stamp()))
 except IOError :
-      sys.exit("\n{}{} {} {}[ERROR]{}  {}  No such file or directory.  Is scrollphat installed on raspberry pi?".format(color.END, __file__, get_line_no(), color.BOLD, color.END, get_time_stamp()))
+      sys.exit("\n{}{} {} {}[ERROR]{}  {}  No such file or directory.  Is scrollphat installed on raspberry pi?".format(color.END, __file__, get_line_no(), color.BOLD, color.END, get_date_stamp()))
 
 # >>>	CUT line ---
 
