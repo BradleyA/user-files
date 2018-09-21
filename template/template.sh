1907d0762acd7fd342c034e8ed5e1fdebb2c0881
Bradley Allen <allen.bradley@ymail.com>
2018-09-21 11:44:07 -0500

    complete DEBUG & ERROR etc with get_date_stamp

--- template/template.sh
+++ template/template.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# 	template.sh  3.37.76  2018-09-21_11:43:32_CDT  https://github.com/BradleyA/user-work-files.git  uadmin  six-rpi3b.cptx86.com 3.36  
+# 	   complete DEBUG & ERROR etc with get_date_stamp 
 # 	template.sh  3.36.75  2018-09-21_11:06:51_CDT  https://github.com/BradleyA/user-work-files.git  uadmin  six-rpi3b.cptx86.com 3.35  
 # 	   change design of DEBUG statement to include date 
 # 	template.sh  2.05.39  2018-09-01_23:42:46_CDT  https://github.com/BradleyA/user-work-files.git  uadmin  six-rpi3b.cptx86.com 2.04  
@@ -29,11 +31,11 @@ echo -e "\nDOCUMENTATION\n   https://github.com/BradleyA/REPOSITORY"
 echo -e "\nEXAMPLES\n   <<your code examples description goes here>>\n\n${0} <<code example goes here>>\n"
 #	After displaying help in english check for other languages
 if ! [ "${LANG}" == "en_US.UTF-8" ] ; then
-	echo -e "${NORMAL}${0} ${LINENO} [${BOLD}WARNING${NORMAL}]:	Your language, ${LANG}, is not supported.\n\tWould you like to help?\n"	1>&2
-#	elif [ "${LANG}" == "fr_CA.UTF-8" ] ; then
-#		echo -e "${NORMAL}${0} ${LINENO} [${BOLD}WARNING${NORMAL}]:	Display help in ${LANG}"	1>&2
-#	else
-#		echo -e "${NORMAL}${0} ${LINENO} [${BOLD}WARNING${NORMAL}]:	Your language, ${LANG}, is not supported.\n\tWould you like to help?\n"	1>&2
+	get_date_stamp ; echo -e "${NORMAL}${0} ${LINENO} ${BOLD}[WARNING]${NORMAL}  ${DATE_STAMP}  Your language, ${LANG}, is not supported.\n\tWould you like to help?\n"	1>&2
+#       elif [ "${LANG}" == "fr_CA.UTF-8" ] ; then
+#               get_date_stamp ; echo -e "${NORMAL}${0} ${LINENO} ${BOLD}[WARNING]${NORMAL}  ${DATE_STAMP}  Display help in ${LANG}"        1>&2
+#       else
+#               get_date_stamp ; echo -e "${NORMAL}${0} ${LINENO} ${BOLD}[WARNING]${NORMAL}  ${DATE_STAMP}  Your language, ${LANG}, is not supported.\n\tWould you like to help?\n" 1>&2
 fi
 }
 
@@ -66,7 +68,7 @@ if [ "${DEBUG}" == "1" ] ; then get_date_stamp ; echo -e "> ${BOLD}DEBUG${NORMAL
 #       Check if argument 1 is blank
 if [ "${1}" == "" ] ; then
         display_help
-        echo -e "${NORMAL}\n${0} ${LINENO} [${BOLD}ERROR${NORMAL}]:  File not found on command line\n" 1>&2
+        get_date_stamp ; echo -e "\n${NORMAL}${0} ${LINENO} ${BOLD}[ERROR]${NORMAL}  ${DATE_STAMP}  File not found on command line\n" 1>&2
         exit 0
 fi
 #       Check if argument 1 is -f or --file
@@ -74,16 +76,16 @@ if [ "${1}" == "--file" ] || [ "${1}" == "-f" ] ; then
         #       Check if argument 2 is blank
         if [ "${2}" == "" ] ; then
                 display_help
-                echo -e "${NORMAL}\n${0} ${LINENO} [${BOLD}ERROR${NORMAL}]:  File name not found; --file <path>/<file_name> or -f <path>/<file_name> option\n" 1>&2
+                get_date_stamp ; echo -e "\n${NORMAL}${0} ${LINENO} ${BOLD}[ERROR]${NORMAL}  ${DATE_STAMP}  File name not found; --file <path>/<file_name> or -f <path>/<file_name> option\n" 1>&2
                 exit 0
         fi
         OPTION2=${2}
 else
-        if [ "${DEBUG}" == "1" ] ; then echo -e "> DEBUG ${LINENO}  VAR  >${VAR}<" 1>&2 ; fi
+        if [ "${DEBUG}" == "1" ] ; then get_date_stamp ; echo -e "> ${BOLD}DEBUG${NORMAL} ${LINENO}  ${DATE_STAMP}  VAR >${VAR}<" 1>&2 ; fi
         #       Check if blank
         if [ "${VAR}" == "" ] ; then
                 display_help
-                echo -e "${NORMAL}\n${0} ${LINENO} [${BOLD}ERROR${NORMAL}]:  ${1} not found; use --file <path>/<file_name> or -f <path>/<file_name> option\n" 1>&2
+                get_date_stamp ; echo -e "\n${NORMAL}${0} ${LINENO} ${BOLD}[ERROR]${NORMAL} ${LINENO}  ${DATE_STAMP}  ${1} not found; use --file <path>/<file_name> or -f <path>/<file_name> option\n" 1>&2
                 exit 0
         fi
 fi
@@ -92,7 +94,7 @@ fi
 #	Check arguement 1 xxx
 if [ "$1" != "no" ] && [ "$1" != "normal" ] && [ "$1" != "all" ] && [ "$1" != "" ] ; then
 	display_help
-	echo -e "${NORMAL}${0} ${LINENO} [${BOLD}ERROR${NORMAL}]:	First arguement, ${1}, is NOT no, normal, all.\n"	1>&2
+	get_date_stamp ; echo -e "${NORMAL}${0} ${LINENO} ${BOLD}[ERROR]${NORMAL} ${LINENO}  ${DATE_STAMP}  First arguement, ${1}, is NOT no, normal, all.\n"	1>&2
 	exit 0
 fi
 ###
