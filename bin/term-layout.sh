d53c42069170e5e922253b66095396aec75665b4
Bradley Allen <allen.bradley@ymail.com>
2019-05-24 15:35:13 -0500

    increate window size

--- bin/term-layout.sh
+++ bin/term-layout.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
+# 	bin/term-layout.sh  3.190.248  2019-05-24T15:35:13.732112-05:00 (CDT)  https://github.com/BradleyA/user-files.git  uadmin  one-rpi3b.cptx86.com 3.189  
+# 	   increate window size 
 # 	bin/term-layout.sh  3.86.126  2018-10-24T21:14:01.008851-05:00 (CDT)  https://github.com/BradleyA/user-work-files.git  uthree  three.cptx86.com 3.85  
 # 	   bin/term-layout.sh add default template.sh code close #3 
 ###
 #	six terminal window 132x43 from lower left to upper right
 ###
-gnome-terminal --geometry=132x42+10+490
-gnome-terminal --geometry=132x42+220+400
-gnome-terminal --geometry=132x42+380+310
-gnome-terminal --geometry=132x42+540+220
-gnome-terminal --geometry=132x42+700+130
-gnome-terminal --geometry=132x42+860+40
+gnome-terminal --geometry=170x42+10+490
+#       gnome-terminal --geometry=132x42+10+490
+gnome-terminal --geometry=158x42+220+400
+#       gnome-terminal --geometry=132x42+220+400
+gnome-terminal --geometry=158x42+380+310
+#       gnome-terminal --geometry=132x42+380+310
+gnome-terminal --geometry=170x42+500+220
+#       gnome-terminal --geometry=132x42+540+220
+gnome-terminal --geometry=170x42+615+130
+#       gnome-terminal --geometry=132x42+700+130
+gnome-terminal --geometry=170x42+793+40
+#       gnome-terminal --geometry=132x42+860+40
 ###
