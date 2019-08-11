b62698ac776c986dc8d30a2c82994bf1aaee8b29
Bradley Allen <allen.bradley@ymail.com>
2019-08-10 19:50:24 -0500

    template/TEST/template.sh/FVT-setup.sh update comments

--- template/TEST/template.sh/FVT-setup.sh
+++ template/TEST/template.sh/FVT-setup.sh
@@ -1,22 +1,31 @@
 #!/bin/bash
-# 	template/TEST/template.sh/FVT-setup.sh  3.221.281  2019-07-28T12:22:51.500414-05:00 (CDT)  https://github.com/BradleyA/user-files.git  uadmin  six-rpi3b.cptx86.com 3.220-1-g337137a  
-# 	   add FVT.setup.sh for symbolic link creation for test case expended output 
+# 	template/TEST/template.sh/FVT-setup.sh  3.245.469  2019-08-10T19:50:24.626724-05:00 (CDT)  https://github.com/BradleyA/user-files.git  uadmin  one-rpi3b.cptx86.com 3.244  
+# 	   template/TEST/template.sh/FVT-setup.sh update comments 
+# 	hooks/FVT-setup.sh  3.244.468  2019-08-10T19:47:00.778853-05:00 (CDT)  https://github.com/BradleyA/user-files.git  uadmin  one-rpi3b.cptx86.com 3.243  
+# 	   hooks/FVT-setup.sh hooks/post-commit move rm -f FVT-*.out from hooks/post-commit to hooks/FVT-setup.sh 
+# 	hooks/FVT-setup.sh  3.243.467  2019-08-10T19:39:36.382334-05:00 (CDT)  https://github.com/BradleyA/user-files.git  uadmin  one-rpi3b.cptx86.com 3.242  
+# 	   hooks/FVT-setup.sh example FVT-setup.sh file to be placed in TEST/<file_name>/ directory with test cases 
+###
+#	FVT-setup.sh -  This script is optional.  It runs before test cases are run, if FVT-setup.sh is found in TEST/<file_name>/
+#
 #### production standard 3.0 shellcheck
 #### production standard 5.1.160 Copyright
 ##       Copyright (c) 2019 Bradley Allen
 ##       MIT License is in the online DOCUMENTATION, DOCUMENTATION URL defined below.
-
-rm -f FVT-options*.out
-#
+###
+#       Remove output from previous run of test cases
+rm -f FVT-*.out
+#	Create symbolic link for all test case expected output that matches another test case expected output 
+#	Expected output from FVT-option-help-001 matches output from FVT-option-help-00[2,3,4,5,6]
 ln -fs FVT-option-help-001.expected FVT-option-help-002.expected
 ln -fs FVT-option-help-001.expected FVT-option-help-003.expected
 ln -fs FVT-option-help-001.expected FVT-option-help-004.expected
 ln -fs FVT-option-help-001.expected FVT-option-help-005.expected
 ln -fs FVT-option-help-001.expected FVT-option-help-006.expected
-#		
+#	Expected output from FVT-option-usage-001 matches output from FVT-option-usage-00[2,3]
 ln -fs FVT-option-usage-001.expected FVT-option-usage-002.expected
 ln -fs FVT-option-usage-001.expected FVT-option-usage-003.expected 
-#
+#	Expected output from FVT-option-version-001 matches output from FVT-option-version-00[2,3]
 ln -fs FVT-option-version-001.expected FVT-option-version-002.expected
 ln -fs FVT-option-version-001.expected FVT-option-version-003.expected
 ###		
